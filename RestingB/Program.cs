aaf702fad634d13d84e756adb2811b693fff1565
Suren Arakelyan <suren.arakelyan@elev.ga.ntig.se>
2024-01-26 12:44:49 +0100

    first commit

---
+++ RestingB/Program.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Text.Json;
+
+RestClient client = new("https://digi-api.com/api/v1/");
+
+string animalname = "digimon";
+RestRequest request = new RestRequest ($"digimon/Aegiochusmon");
+
+RestResponse response = client.GetAsync(request).Result;
+
+Digimon d = JsonSerializer.Deserialize<Digimon>(response.Content);
+
+Console.WriteLine(d.Name);
+Console.WriteLine(d.Id);
+Console.WriteLine(d.Attribute);
+
+
+Console.ReadLine(); 
+
+
+
+
+Console.ReadLine(); 
+
